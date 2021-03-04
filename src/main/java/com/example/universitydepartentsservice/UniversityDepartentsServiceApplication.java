a6d9414a8cc52feceeef620a7f0302e0133db323
antonpanchishko <PanchishkoMayor@gmail.com>
2021-03-04 21:22:15 +0200

    first commit

---
+++ src/main/java/com/example/universitydepartentsservice/UniversityDepartentsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitydepartentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityDepartentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityDepartentsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/universitydepartentsservice/UniversityDepartentsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitydepartentsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityDepartentsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
